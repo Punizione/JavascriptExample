f020d69014d7071e25e224e9c770506f3ae5eeac
winkboy <43905904+winkboyleung@users.noreply.github.com>
2019-09-03 00:34:38 +0800

    Update 破解上海法院网.py

--- 破解上海法院网.py
+++ 破解上海法院网.py
@@ -1,22 +1,23 @@
-import requests
-import PyV8, re
+import requests, re, execjs
+
 
 headers = {
-    'Host' : 'shfy.chinacourt.org',
+    'Host' : 'shfy.chinacourt.gov.cn',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
     'Referer': 'http://shfy.chinacourt.org/paper/more/paper_mid/MzA0gAMA.shtml'
 }
 
 # 文章url
-url = 'http://shfy.chinacourt.org/paper/detail/2018/11/id/1927107.shtml'
+url = 'http://shfy.chinacourt.gov.cn/paper/detail/2018/11/id/1927107.shtml'
 
 resp = requests.get(url, headers = headers)
 content = resp.content.decode()
+# print(content)
 # 获取到文章的加密字段
-encode_content = re.search(r'(var tm=new.*?html=\'\';)', content, re.S).group(1)
+encode_content = re.search(r'tm\[0\]="(.*?)";var', content, re.S).group(1)
+# print(encode_content)
 
-
-
+# 下面js代码通过上面的content返回值找到加密字段是通过paperDecode的js文件来处理的、然后搜索到该js(混淆js)，需要通过解密(需要解密两次)获取到最终的js代码
 hehe = '''
 function disableCopy() {
 	return false
@@ -24,7 +25,6 @@ function disableCopy() {
 // document.body.onselectstart = disableCopy;
 // document.body.oncopy = disableCopy;
 // document.body.oncut = disableCopy;
-
 function paperDecode(paperString) {
 	var ret = '';
 	paperString = unescape(paperString);
@@ -36,25 +36,9 @@ function paperDecode(paperString) {
 function jsPaperDecode(paperString) {
 	document.write(paperDecode(paperString))
 }
-
-''' + eval('"{}"').format(encode_content) + '''
-
-show=function(){
-			html+=paperDecode(tm[n]);
-			// document.getElementById('border_in').innerHTML+='. ';
-			n++;
-			if(n<tm.length){
-				setTimeout(show,50);
-			}else{
-				// document.getElementById('border_in').innerHTML=html;
-				return html;
-			}
-		}
-show();
 '''
-# ''' + eval('"{}"').format(encode_content) + '''
-# 上面这行代码的意思是先将正则匹配到的加密文本合并到js中、由于合并后的这个正则有双引号存在、如果直接运行则会报错、所以使用eval函数去掉
-execute = PyV8.JSContext()
-execute.enter()
-result = execute.eval(hehe)
+
+# 最后只需要将刚刚获取到的加密文本作为参数、传入该js函数中、所得的返回值便是解密后的数据
+ctx = execjs.compile(hehe)
+result = ctx.call('paperDecode', encode_content)
 print(result)
